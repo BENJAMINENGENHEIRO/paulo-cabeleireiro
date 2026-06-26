c7334317ca2451618898e588b661914b50dba5ed
BENJAMINENGENHEIRO <shoppingdetodos2000@gmail.com>
2026-06-26 18:19:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'paulo-cabeleireiro-v1';
+const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
